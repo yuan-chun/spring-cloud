e777a0a2fbdc240ebf98dc6e32697c6173694dcd
yuan-chun <1310444316@qq.com>
2018-12-14 09:52:13 +0800

    ribbon本地负载均衡 order callservice member 注意： 1,order工程启动类中 @bean 注入 RestTemplate ，并 @LoadBalanced 开启ribbon 2,order调用member时调用url http://member注册到eureka的服务名/接口映射

--- service-member/src/main/java/com/yuanchun/member/api/service/MemberService.java
+++ service-member/src/main/java/com/yuanchun/member/api/service/MemberService.java
@@ -1,14 +1,17 @@
 package com.yuanchun.member.api.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberService {
+    @Value("${server.port}")
+    private String port ;
 
     @RequestMapping("/getMemberService")
     public String getMemberService(){
 
-        return "this is service-member";
+        return "this is service-member and port = " + port;
     }
 }
--- service-oeder/src/main/java/com/yuanchun/oeder/AppOederService.java
+++ service-oeder/src/main/java/com/yuanchun/oeder/AppOederService.java
@@ -2,12 +2,21 @@ package com.yuanchun.oeder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class AppOederService {
     public static void main(String[] args) {
         SpringApplication.run(AppOederService.class,args);
+    }
+
+    @Bean//ioc注入
+    @LoadBalanced//开启ribbon负载均衡
+    RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
