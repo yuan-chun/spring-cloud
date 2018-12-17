1a6adbe91018b764dc234e94ce900004665be508
yuan-chun <1310444316@qq.com>
2018-12-17 22:17:02 +0800

    测试config server 获取配置文件

--- config-client/src/main/java/com/yuanchun/config/client/controller/GetController.java
+++ config-client/src/main/java/com/yuanchun/config/client/controller/GetController.java
@@ -10,16 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中。
 public class GetController {
 
-    @Value("${eureka.instance.lease-renewal-interval-in-seconds}")
+    @Value("${neo.hello}")
     private String eilriis;
 
-    @Value("${eureka.client.serviceUrl.defaultZone}")
-    private String ecsd;
 
     @RequestMapping("/getFromConfigServer")
     public String getFromConfigServer(){
-        return "eureka.instance.lease-renewal-interval-in-seconds = "+eilriis
-                +"  eureka.client.serviceUrl.defaultZone"  + ecsd;
+        return "eureka.instance.lease-renewal-interval-in-seconds = "+eilriis;
     }
 
 }
