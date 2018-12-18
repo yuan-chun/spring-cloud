37f3af9266b62233029e504488e89948fa9c5d82
yuan-chun <1310444316@qq.com>
2018-12-18 08:56:10 +0800

    去除无用import

--- config-client/src/main/java/com/yuanchun/config/client/AppConfigClient.java
+++ config-client/src/main/java/com/yuanchun/config/client/AppConfigClient.java
@@ -3,11 +3,9 @@ package com.yuanchun.config.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-//配置中心使用eureka注册到注册中心后，配置文件会从远程读取
 //因此读取configService会端口冲突
 public class AppConfigClient {
     public static void main(String[] args) {
