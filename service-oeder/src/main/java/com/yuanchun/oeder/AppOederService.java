3edb046b009c782867f1dc7675ca9ff2899600df
yuanchun_asiainfo <yuanchun@asiainfo.com>
2020-04-11 14:13:10 +0800

    DiscoveryClient 暴露服务信息

--- service-oeder/src/main/java/com/yuanchun/oeder/AppOederService.java
+++ service-oeder/src/main/java/com/yuanchun/oeder/AppOederService.java
@@ -2,6 +2,7 @@ package com.yuanchun.oeder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableDiscoveryClient
 public class AppOederService {
     public static void main(String[] args) {
         SpringApplication.run(AppOederService.class,args);
